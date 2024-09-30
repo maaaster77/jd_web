077eda136974ec3aaf5300902e8500cc3cc87e5e
胡作超 <hu.zuochao@qianka.com>
2024-09-30 16:55:10 +0800

    fix

--- start.sh
+++ start.sh
@@ -19,14 +19,16 @@ else
 fi
 
 # 停止tg聊天抓取job
-pid=$(ps aux | grep 'chat_history' | grep -v grep | awk '{print $2}')
-if [ -z "$pid" ]; then
-  echo "聊天抓取脚本 未运行"
-else
-  kill -9 $pid
-  echo "已关闭 聊天抓取脚本 进程: $pid"
-fi
+#pid=$(ps aux | grep 'chat_history' | grep -v grep | awk '{print $2}')
+#if [ -z "$pid" ]; then
+#  echo "聊天抓取脚本 未运行"
+#else
+#  kill -9 $pid
+#  echo "已关闭 聊天抓取脚本 进程: $pid"
+#fi
 
+echo "启动celery beat"
+nohup celery -A scripts.worker:celery beat  --loglevel=info log/celery_beat.txt 2>&1 &
 
 # 启动celery
 echo "启动celery first"
@@ -35,8 +37,7 @@ nohup celery -A scripts.worker:celery worker -Q jd.celery.first --loglevel=info 
 echo "启动celery telegram"
 nohup celery -A scripts.worker:celery worker -Q jd.celery.telegram -c 1 --loglevel=info > log/celery_telegram_out.txt 2>&1 &
 
-echo "启动celery beat"
-nohup celery -A scripts.worker:celery beat  --loglevel=info log/celery_beat.txt 2>&1 &
+
 
 
 # echo "启动flower"
@@ -49,7 +50,7 @@ nohup python -m web   > log/flask_out.txt 2>&1 &
 echo "web: http://127.0.0.1:8981"
 
 # # 启动tg聊天记录抓取ojb
-echo "启动tg聊天记录抓取ojb"
-nohup python -m scripts.job chat_history > log/chat_history.txt 2>&1 &
+#echo "启动tg聊天记录抓取ojb"
+#nohup python -m scripts.job chat_history > log/chat_history.txt 2>&1 &
 
 # echo 'End'
