2170ce61e62c7fdfa486b779b6b59291309e29f9
Super-H1 <66242506+Super-H1@users.noreply.github.com>
2024-10-10 15:52:51 +0800

    job log (#58)
    * job log
    
    * job
    
    ---------
    
    Co-authored-by: 胡作超 <hu.zuochao@qianka.com>

---
+++ jd/models/job_queue_log.py
@@ -0,0 +1,15 @@
+from jd import db
+
+
+class JobQueueLog(db.Model):
+    __tablename__ = 'chemical_platform_product_info'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(126), nullable=False, unique=False, default='', comment='job name')
+    status = db.Column(db.Integer, nullable=False, default=0, comment='状态 0-待处理 1-处理中 2-已处理')
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+
+    class StatusType:
+        NOT_START = 0
+        RUNNING = 1
+        FINISHED = 2
