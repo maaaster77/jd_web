86d4fd739006dd6ab1153fd6c1cbeecff7e246b3
Super-H1 <66242506+Super-H1@users.noreply.github.com>
2024-11-01 14:41:18 +0800

    fix (#88)
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: 胡作超 <hu.zuochao@qianka.com>

--- jd/jobs/once/send_tg_data.py
+++ jd/jobs/once/send_tg_data.py
@@ -2,13 +2,16 @@ from jd.tasks.first.send_file_job import send_file_job
 
 
 class SendTgDataJob:
+
+    def __init__(self, data_type):
+        self.data_type = int(data_type)
+
     def main(self):
-        send_file_job()
+        send_file_job(self.data_type)
 
 
-
-
-
-def run():
-    job = SendTgDataJob()
+def run(data_type):
+    if not data_type:
+        raise ValueError('data type is required')
+    job = SendTgDataJob(data_type)
     job.main()
