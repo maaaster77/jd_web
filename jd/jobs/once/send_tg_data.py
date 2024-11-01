b7db8ed11e64fbbd24288f17a995f8124d23f92a
胡作超 <hu.zuochao@qianka.com>
2024-11-01 15:33:07 +0800

    fix

--- jd/jobs/once/send_tg_data.py
+++ jd/jobs/once/send_tg_data.py
@@ -3,15 +3,18 @@ from jd.tasks.first.send_file_job import send_file_job
 
 class SendTgDataJob:
 
-    def __init__(self, data_type):
+    def __init__(self, data_type, is_all):
         self.data_type = int(data_type)
+        self.is_all = int(is_all)
 
     def main(self):
-        send_file_job(self.data_type)
+        send_file_job(self.data_type, self.is_all)
 
 
-def run(data_type):
+def run(data_type, is_all):
     if not data_type:
         raise ValueError('data type is required')
-    job = SendTgDataJob(data_type)
+    if not is_all:
+        raise ValueError('is_all is required 1-all 0-yesterday')
+    job = SendTgDataJob(data_type, is_all)
     job.main()
