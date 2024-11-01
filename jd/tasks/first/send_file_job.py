10a7a9fbf1b5ffc22dbe0879e732c496c873bdb0
Super-H1 <66242506+Super-H1@users.noreply.github.com>
2024-11-01 15:33:52 +0800

    fix (#89)
    Co-authored-by: 胡作超 <hu.zuochao@qianka.com>

--- jd/tasks/first/send_file_job.py
+++ jd/tasks/first/send_file_job.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 
 
 @celery.task
-def send_file_job(data_type=0):
+def send_file_job(data_type=0, is_all=0):
     """
     tg_group tg_group_chat_history tg_group_use_info
 
@@ -35,7 +35,7 @@ def send_file_job(data_type=0):
     else:
         model_list = [TgGroup, TgGroupChatHistory, TgGroupUserInfo]
     for model in model_list:
-        add_list, update_list = deal_data(model)
+        add_list, update_list = deal_data(model, is_all)
         file_list.extend(add_list)
         file_list.extend(update_list)
     FtpService.send_file_by_file_path(file_list)
@@ -43,23 +43,22 @@ def send_file_job(data_type=0):
     logger.info('ftp send file job end...')
 
 
-def deal_data(model: Type[TgGroup, TgGroupChatHistory, TgGroupUserInfo]):
+def deal_data(model: Type[TgGroup, TgGroupChatHistory, TgGroupUserInfo], is_all):
     last_id = 0
     file_list = []
     update_file_list = []
     now_time = datetime.datetime.now()
-    tmp_data_end_time = datetime.datetime(2024, 11, 1, 23, 59, 59)
-    if now_time.strftime('%Y%m%d') == (tmp_data_end_time + datetime.timedelta(days=1)).strftime('%Y%m%d'):
-        # 这一天不处理，下一天才开始处理
-        return [], []
     yesterday = now_time - datetime.timedelta(days=1)
     start_time = yesterday.strftime('%Y-%m-%d 00:00:00')
     end_time = yesterday.strftime('%Y-%m-%d 23:59:59')
+    max_id = model.query.order_by(model.id.desc()).first().id
     while True:
         query = model.query.filter(model.id > last_id)
-        if now_time > tmp_data_end_time:
-            # 1号开始，处理昨天的数据
+        if not is_all:
+            # 不处理全部的，只处理昨天的数据
             query = query.filter(model.created_at.between(start_time, end_time))
+        else:
+            query = query.filter(model.id <= max_id)
         rows = query.order_by(model.id.asc()).limit(1000).all()
         if not rows:
             break
@@ -81,4 +80,4 @@ def deal_data(model: Type[TgGroup, TgGroupChatHistory, TgGroupUserInfo]):
 
 if __name__ == '__main__':
     app.ready(db_switch=True, web_switch=False, worker_switch=True)
-    send_file_job()
+    send_file_job(is_all=1)
