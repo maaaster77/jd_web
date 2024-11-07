0dc90737d4c4c33d58b442821d16f85475b250c4
胡作超 <hu.zuochao@qianka.com>
2024-11-07 09:24:57 +0800

    stop job

--- jCelery/celeryconfig.py
+++ jCelery/celeryconfig.py
@@ -21,12 +21,12 @@ beat_schedule = {
         'task': 'jd.tasks.first.tg_history_job.fetch_tg_history_job',
         'schedule': timedelta(minutes=10),
     },
-    'tg_account_history_job': {
-        'task': 'jd.tasks.first.tg_history_job.fetch_account_history_job',
-        'schedule': timedelta(minutes=45),
-    },
-    'send_file_job': {
-        'task': 'jd.tasks.first.send_file_job.send_file_job',
-        'schedule': crontab(minute=0, hour=9),
-    }
+    # 'tg_account_history_job': {
+    #     'task': 'jd.tasks.first.tg_history_job.fetch_account_history_job',
+    #     'schedule': timedelta(minutes=45),
+    # },
+    # 'send_file_job': {
+    #     'task': 'jd.tasks.first.send_file_job.send_file_job',
+    #     'schedule': crontab(minute=0, hour=9),
+    # }
 }
